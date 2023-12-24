a7a1e022418f0aaf373e1fc21cd8b70ce12559ed
Brad Williams <44789386+Brad-Williams-Dev@users.noreply.github.com>
2023-12-24 09:49:36 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="christmas-lights">Lucas & Nolan's Santa Tracker</h1>
+                <h1 className="christmas-lights">Colton's Santa Tracker</h1>
                 <p className="greeting">Let's find out where Santa is!</p>
                 <div className="santa-tracker">
                     <iframe src="https://www.noradsanta.org/embed.html" allowFullScreen mozAllowFullScreen webkitAllowFullScreen></iframe>
